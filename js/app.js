cf65832f186936904752ae9d0bb08cc1c6b0668c
tsgnazu <cjisironmanv3@gmail.com>
2018-12-17 03:45:31 -0700

    App Upload For Release
    App Upload For Release

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var $ = Dom7;
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+    theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+var app = new Framework7({
+    id: 'io.framework7.testapp',
+    root: '#app',
+    theme: theme,
+    data: function () {
+        return {
+            user: {
+                firstName: 'John',
+                lastName: 'Doe',
+            },
+        };
+    },
+    methods: {
+        helloWorld: function () {
+            app.dialog.alert('Hello World!');
+        },
+    },
+    routes: routes,
+    vi: {
+        placementId: 'pltd4o7ibb9rc653x14',
+    },
+});
